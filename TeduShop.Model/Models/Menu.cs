80344481cd1b6a7673cdf82e207fb42e3be8bfb9
viethieule <viethieule@gmail.com>
2018-11-19 18:39:32 +0700

    the first commit

---
+++ TeduShop.Model/Models/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string URL { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        public int GroupID { get; set; }
+
+        [ForeignKey("GroupID")] // Tên trường mà GroupID ở trên sẽ trỏ đến trong bảng MenuGroup. Bên Menu Group cần có một IEnumeable<Menu>
+        public virtual MenuGroup MenuGroup { get; set; }
+
+        [StringLength(50)]
+        public string Target { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TeduShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TeduShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31120a5e-47a4-4d95-800a-9408fb26a2f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
