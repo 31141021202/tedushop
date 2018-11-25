a599c2323cdd99bed82642f010d8fbf6449b5bd6
viethieule <viethieule@gmail.com>
2018-11-25 13:35:49 +0700

    add unit tests for repository and service

--- TeduShop.Service/PostCategoryService.cs
+++ TeduShop.Service/PostCategoryService.cs
@@ -7,8 +7,8 @@ namespace TeduShop.Service
 {
     public interface IPostCategoryService
     {
-        void Add(PostCategory postCategory);
-        void Delete(int id);
+        PostCategory Add(PostCategory postCategory);
+        PostCategory Delete(int id);
         void Update(PostCategory postCategory);
         IEnumerable<PostCategory> GetAll();
         IEnumerable<PostCategory> GetAllByParentId(int parentId);
@@ -25,14 +25,14 @@ namespace TeduShop.Service
             this._unitOfWork = unitOfWork;
         }
 
-        public void Add(PostCategory postCategory)
+        public PostCategory Add(PostCategory postCategory)
         {
-            _postCategoryRepository.Add(postCategory);
+            return _postCategoryRepository.Add(postCategory);
         }
 
-        public void Delete(int id)
+        public PostCategory Delete(int id)
         {
-            _postCategoryRepository.Delete(id);
+            return _postCategoryRepository.Delete(id);
         }
 
         public IEnumerable<PostCategory> GetAll()
