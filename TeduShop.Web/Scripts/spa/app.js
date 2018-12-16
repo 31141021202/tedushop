92686ead74a415b4404071f4bf3763b3b41b698f
viethieule <viethieule@gmail.com>
2018-12-16 12:55:38 +0700

    what is scope, root scope and nested scope

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,23 @@
+﻿var myApp = angular.module('myModule', []);
+
+// create the controller and register the controller with the module
+
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+// myController.$inject = ['$scope'];
+
+// declare the controller
+
+function schoolController($scope) {
+    $scope.message = "Announcement fron School";
+}
+
+function studentController($scope) {
+    $scope.message = "This is my message from Student Controller";
+}
+
+function teacherController($scope) {
+    $scope.message = "This is my message from Teacher Controller";
+}
