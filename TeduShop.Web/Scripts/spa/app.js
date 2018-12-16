2fbf83bc2937fa8c1c4f61a7279c4b0e61664e6e
viethieule <viethieule@gmail.com>
2018-12-16 16:24:45 +0700

    service in angularJS

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -1,23 +1,57 @@
 ﻿var myApp = angular.module('myModule', []);
 
-// create the controller and register the controller with the module
+// register the controller with the module (also create the ctrlr)
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
 
-// myController.$inject = ['$scope'];
+// similarly, register the service
 
-// declare the controller
+myApp.service('Validator', Validator);
 
-function schoolController($scope) {
-    $scope.message = "Announcement fron School";
+schoolController.$inject = ['$scope', 'Validator'];
+
+function schoolController($scope, Validator) {
+    //$scope.message = "Announcement fron School";
+
+    // binding event giữa view controller
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+
+    $scope.num = 1;
 }
 
-function studentController($scope) {
-    $scope.message = "This is my message from Student Controller";
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+        // sau dấu : là tên phương thức để thao tác bên ngoài, có thể viết giống nhau như set, get ấy
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else {
+            return 'This is odd';
+        }
+            
+    }
 }
 
-function teacherController($scope) {
-    $scope.message = "This is my message from Teacher Controller";
-}
+// Example for scope, rootScope and nested scope
+
+//myApp.controller("studentController", studentController);
+//myApp.controller("teacherController", teacherController);
+
+//// myController.$inject = ['$scope'];
+
+//// declare the controller
+
+
+
+//function studentController($scope) {
+//    $scope.message = "This is my message from Student Controller";
+//}
+
+//function teacherController($scope) {
+//    $scope.message = "This is my message from Teacher Controller";
+//}
