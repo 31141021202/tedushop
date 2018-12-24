52a7f5536d753ce2ceded707a74aeab0fa1f9f9d
viethieule <viethieule@gmail.com>
2018-12-24 15:39:48 +0700

    add status filter and format data in list

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,9 @@
+﻿(function (app) {
+    app.filter('statusFilter', statusFilter);
+
+    function statusFilter() {
+        return function (input) {
+            return input ? 'Kích hoạt' : 'Khóa';
+        }
+    };
+})(angular.module('tedushop.common'));
