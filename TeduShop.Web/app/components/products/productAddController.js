15cbd9f7d11ade1700cf5ca2640b4bde6aafbe5f
viethieule <viethieule@gmail.com>
2018-12-29 22:15:41 +0700

    Revert "add product management & ckeditot, ckfinder"
    This reverts commit c6ffd002f2fedd3595e348e6fbec82a5030cb070.

--- TeduShop.Web/app/components/products/productAddController.js
+++ TeduShop.Web/app/components/products/productAddController.js
@@ -1,54 +1,7 @@
 ﻿(function (app) {
     app.controller('productAddController', productAddController);
 
-    productAddController.$inject = ['$scope', 'apiService', 'notificationService', '$state', 'commonService'];
+    function productAddController() {
 
-    function productAddController($scope, apiService, notificationService, $state, commonService) {
-        $scope.product = {
-            CreatedDate: new Date(),
-            Status: true
-        };
-
-        $scope.ckeditorOptions = {
-            language: 'vi',
-            height: '200px'
-        };
-
-        $scope.GetSeoTitle = GetSeoTitle;
-
-        function GetSeoTitle() {
-            $scope.product.Alias = commonService.getSeoTitle($scope.product.Name);
-        }
-
-        $scope.productCategories = [];
-
-        $scope.AddProduct = AddProduct;
-
-        function AddProduct() {
-            apiService.post('api/product/create', $scope.product, function (result) {
-                notificationService.displaySuccess(result.data.Name + ' đã được thêm mới.');
-                $state.go('products');
-            }, function (error) {
-                notificationService.displayError('Thêm mới không thành công.');
-            });
-        }
-
-        function loadProductCategories() {
-            apiService.get('api/productcategory/getallparents', null, function (result) {
-                $scope.productCategories = result.data;
-            }, function () {
-                console.log('Cannot get category');
-            });
-        }
-
-        $scope.ChooseImage = function () {
-            var finder = new CKFinder();
-            finder.selectActionFunction = function (fileUrl) {
-                $scope.product.Image = fileUrl;
-            }
-            finder.popup();
-        }
-
-        loadProductCategories();
     }
 })(angular.module('tedushop.products'));
